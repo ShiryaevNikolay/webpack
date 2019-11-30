7d00d65b883aa96e89d105a331c7d3a158566281
ShiryaevNikolay <deendeeps@list.ru>
2019-11-30 16:10:33 +0300

    Fourth video lesson. Connected Vue and it works

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import './css/main.css'
 import './scss/main.scss'
 
 window.Vue = require('vue')
+
+Vue.component('example-component', require('./components/Example.vue').default)
+
+const app = new Vue({
+    el: '#app'
+})
