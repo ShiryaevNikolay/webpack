75ff040a6d4e6a1175d297c23ae108ae7a52fe3f
ShiryaevNikolay <deendeeps@list.ru>
2019-11-30 12:12:27 +0300

    Second video lesson

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
@@ -15,11 +16,42 @@ module.exports = {
             loader: 'babel-loader',
             exclude: '/node_modules/'
         }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader, 
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                }, {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                }, {
+                    loader: 'sass-loader',
+                    options: { sourceMap: true }
+                }
+            ],
+        }, {
             test: /\.css$/,
-            loader: 'css-loader'
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader, 
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                }, {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                }
+            ],
         }]
     },
     devServer: {
         overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css'
+        }),
+      ]
 }
